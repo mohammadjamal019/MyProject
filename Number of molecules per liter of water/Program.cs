0bff47cb9ded469d8d2ea7953d3ac8aa28d47dd8
Mohammed Jamalzehi <63859288+mohammadjamal019@users.noreply.github.com>
2022-05-19 16:16:53 +0430

    Number of molecules per liter of water
    a code for counting the number of molecules per liter of water

---
+++ Number of molecules per liter of water/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace P 
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      double m = 3.0e-23, l = 950, count;
+      double w;
+      Console.Write("enter a weight (Liters): ");
+      w = Convert.ToDouble(Console.ReadLine());
+
+      count = (w * l) / m;
+
+      Console.WriteLine($"  the count is: {count} ");
+      Console.ReadKey();
+
+    }
+  }
+}
